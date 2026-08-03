b6dc2bf917428e27ae35a5f7059555e167cdd76c
Swati kashyap <swatikashyap9233@gmail.com>
2026-08-03 10:05:56 -0700

    Add files via upload

---
+++ lecture_02.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+// int main(){
+//    int ch;
+//    cin>>ch;
+
+//    cout<<(ch>=5?"larger":"smaller")<<endl;
+   
+//     return 0;
+// }
+
+
+
+// int main(){
+//     int n;
+//     cout<<"Enter n: ";
+//     cin>>n;
+//     int sum=0;
+//     int count =1;
+//     for(count =1;count<=n;count++){
+//         if(count%2!=0){
+//             sum=sum+count;
+//         }
+//     }
+//     cout<<"sum: "<<sum;
+//     return 0;
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter n: ";
+//     cin>>n;
+
+//     bool isprime=true;
+//     if(n<=1){
+//         cout<<"Not prime";
+//     }
+//     for(int i=2;i<n;i++){
+//         if(n%i==0){
+//             isprime=false;
+//         }
+//     }
+//     if(isprime){
+//         cout<<"Prime";
+//     }
+//     else{
+//         cout<<"Not Prime";
+//     }
+//     return 0;
+// }
