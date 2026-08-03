e4f4b10232230d54250118b0677381de92f9d4e8
Swati kashyap <swatikashyap9233@gmail.com>
2026-08-03 10:11:50 -0700

    Add files via upload

---
+++ bubble_sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter no. of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "enter" << n << "elements";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    for(int i=0; i<n-1;i++){
+        for(int j=0; j<n-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    cout << "sorted array is: ";
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
