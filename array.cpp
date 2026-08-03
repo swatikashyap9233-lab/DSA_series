8e2c734f025b8bb29444d7017094dbab6d6630f5
Swati kashyap <swatikashyap9233@gmail.com>
2026-08-03 10:10:39 -0700

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,67 @@
+// #include <bits/stdc++.h>>
+// using namespace std;
+// int main() {
+    // smallest in the array
+    // int smallest = INT_MAX;            // + infinity in c++
+    // int largest = INT_MIN;             // - infinity in c++
+    // int num [5] = {8,2,3,4,5};
+    // for (int i = 0; i<5; i++){
+    //     if (num[i] < smallest)
+    //     smallest = num[i];
+    // }
+    // for(int i =0; i<5; i++){
+    //     if (num[i] > largest)
+    //     largest = num[i];
+    // }
+
+    // we can also use this as min and max 
+
+//     for(int i =0; i<5; i++){
+//         smallest = min(num[i], smallest);
+//         largest = max(num[i], largest);
+//     }
+//     cout << smallest << endl;
+//     cout << largest << endl;
+//     return 0;
+
+// }
+
+// change of array
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void changeArr(int arr[], int size){
+//     for(int i=0; i<size; i++){
+//         arr[i] = 2 * arr[i];
+//     }
+// }
+// int main() {
+// int arr[] = {1,2,3};
+// changeArr(arr, 3);
+// for(int i =0; i<3; i++){
+//     cout << arr[i];
+// }
+// cout << endl;
+// }
+
+// LINEAR SEARCH
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int linSrch(int arr[], int n){
+//     for(int i=0; i<5; i++){
+//         if (arr[i] == n){
+//             return i;
+//         }
+//  }
+//  return -1;
+// }
+// int main(){
+//     int arr[] = {8, 10, 4, 1, 2};
+//     cout << linSrch(arr, 10) << endl;
+//     return 0;
+// }
+
+
+
+
