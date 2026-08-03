281cc52e144a1875dad32778abe394d0dabc004c
Swati kashyap <swatikashyap9233@gmail.com>
2026-08-03 10:12:36 -0700

    Add files via upload

---
+++ chatgpt_prob.cpp
@@ -0,0 +1,86 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n= 3;
+//     for (int i=0; i<n; i++){
+//         for (int j=0; j<2-i; j++){
+//             cout << " ";
+//         }
+//         for (int j =0; j<2*i+1; j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+
+//     for (int i= 0; i<2; i++){
+//         for (int j= 0; j<i+1; j++){
+//             cout << " ";
+//         }
+//         cout << "*";
+//         for (int j=1; j<2*(n-i)-3; j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main () {
+//     int n = 3;
+//     for(int i = 0; i<n; i++){
+//         for(int j=0; j<i; j++){
+//             cout << " ";
+//         }
+//         for(int j=0; j<2*(n-i)-1; j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+//min of 2 number
+// int min(int a, int b){
+//     if(a<b){
+//         return a;
+//     }
+//     else {
+//         return b;
+//     }
+// }
+
+// int main() {
+//     cout << min(11, 10) << endl;
+//     return 0;
+// }
+
+// sum of 1 to N
+// #include <bits/stdc++.h>
+// using namespace std;
+// int sumOfNum(int n){
+//     return n + ((n-1)*n)/2;
+// }
+
+// int main (){
+//     cout << sumOfNum(5) << endl;
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int fac(int n){
+//     int mult =  1;
+//     for(int i =1; i<=n; i++){
+//         mult *=i;
+//     }
+//     return mult;
+// }
+// int main(){
+//     cout << fac(4) << endl;
+//     return 0;
+// }
+
