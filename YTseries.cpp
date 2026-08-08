6f6b9cf05a0160390f20b770152398d966067940
Swati kashyap <swatikashyap9233@gmail.com>
2026-08-08 02:30:49 -0700

    Add files via upload

---
+++ YTseries.cpp
@@ -0,0 +1,81 @@
+// #include <iostream>
+// using namespace std;
+// int main () {
+
+//     int n;
+//     cout << "Enter any number: ";
+//     cin >> n;
+//     int sum = 0;
+//     for (int i=1;i<=n;i++){
+//         if (i==5){
+//             break;
+//         }
+//         sum += i;
+//     }
+//     cout << "the sum is: " << sum;
+    // for (int i = 1; i<=5; i++){
+    //     cout << i << " ";
+    // }
+    // return 0;
+    // int i = 1;
+    // // print from 1 to 5
+    // while (i<6){
+    //     cout << i <<" ";
+    //     i++;
+    // }
+    // return 0;
+// }
+//     char alpha;
+//     cout << "Enter any character: ";
+//     cin >> alpha;
+//     if (alpha >='a' && alpha <='z') {
+//         cout << "lowercase";
+//     }
+//     else {
+//         cout <<"uppercase";
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main () {
+//     // check prime or not
+//     int n;
+//     cout << "Enter any no.: ";
+//     cin >> n;
+//     if(n<=1){
+//         cout << "Not a prime no.";
+//         return 0;
+//     }
+//     bool isPrime = true;
+//     for (int i=2; i <n; i++){
+        // yha loop ka use kr rhe h agar hmlog pehle 
+        // hi prime declare kr denge the loop break ho jayega
+        // aur condition fulfill nii ho payega isiliye hmlogo 
+        // ko not prime ka condition likhna hoga 
+//         if (n%i == 0){
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if (isPrime) {
+//         cout << "A prime no.";
+//     }
+//     else{
+//         cout << "Not a prime no.";
+//     }
+//     return 0;
+// }
+//     int n;
+//     int sum = 0;
+//     cout << "Enter any random no.: ";
+//     cin >> n;
+//     // sum of 1 to N odd numbers
+//     for (int i = 1; i<=n; i++) {
+//         if (i%2 != 0){
+//         sum += i;
+//     }
+// }
+//     cout << "The sum is: " << sum;
+
