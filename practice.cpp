f567486bf1486c2d9ff00dc40f68c7fe80584d26
Swati kashyap <swatikashyap9233@gmail.com>
2026-09-06 23:35:46 -0700

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+// class Student{
+//     public:
+//     int rollNo;
+//     string name;
+
+//     void display(){
+//         cout<<"Roll No.: "<<rollNo<<endl;
+//         cout<<"Name: "<<name<<endl;
+//     }
+// };
+
+// int main(){
+//     Student s;
+//     s.name="abc";
+//     s.rollNo=101;
+
+//     Student *ptr=&s;
+//     ptr->display();
+//     return 0;
+// }
+
+// class Employee{
+//     public:
+//     int id;
+//     string name;
+
+//     void display(){
+//         cout<<"ID: "<<id<<endl;
+//         cout<<"Name: "<<name<<endl;
+//     }
+// };
+
+// int main(){
+//     Employee *emp=new Employee;
+//     emp->id=101;
+//     emp->name="Dilip";
+
+//     emp->display();
+//     delete emp;
+
+//     return 0;
+// }
