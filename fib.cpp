883fb315adafddd9c369c2c62dc698062ff85e8a
Swati kashyap <swatikashyap9233@gmail.com>
2026-09-06 23:32:54 -0700

    Add files via upload

---
+++ fib.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n){
+    if(n<=1){
+        return n;
+    }
+    return fibonacci(n-1)+fibonacci(n-2);
+}
+
+int main(){
+    int x;
+    cout<<"x: ";
+    cin>>x;
+    cout<<"Fibonacci: "<<fibonacci(x);
+    return 0;
+}
