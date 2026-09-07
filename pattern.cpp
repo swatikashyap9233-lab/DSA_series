3556259890fbc865812043811b25c8d9f66ea65d
Swati kashyap <swatikashyap9233@gmail.com>
2026-09-06 23:46:47 -0700

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,40 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n = 4;
+//     int i;
+//     for(int i=0; i<n; i++){
+//         // space
+//         for(int j= 0;j< n-1-i; j++){
+//             cout << " ";
+//         }
+//         cout << "*";
+    
+//     if (i!=0){
+//         // space
+//         for(int j=0; j<(2*i-1); j++){
+//             cout << " ";
+//         }
+//         cout << "*";
+//     }
+//     cout << endl;
+// }
+
+// // bottom
+// for(int i = 0; i<3; i++){
+//     //space
+//     for(int j= 0;j<i+1; j++){
+//         cout << " ";
+//     }
+//     cout <<"*";
+
+//     if (i!=2){
+//         for (int j=0; j<2*(n-i)-5; j++){
+//             cout << " ";
+//         }
+//         cout << "*";
+//     }
+//     cout << endl;
+// }
+// return 0;
+// }
