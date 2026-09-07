7258d5be2ce412d72543b5d9bbb137129d1fb177
Swati kashyap <swatikashyap9233@gmail.com>
2026-09-06 23:30:48 -0700

    Add files via upload

---
+++ ObjectAsFunArg.cpp
@@ -0,0 +1,47 @@
+// #include <iostream>
+// using namespace std;
+
+// class student {
+// public:
+//     int marks;
+// };
+
+// void display(const student& s) {
+//     cout << s.marks;
+// }
+
+// int main() {
+//     student s;
+//     s.marks = 90;
+
+//     display(s);
+
+//     return 0;
+// }
+
+
+
+// objects returned from functions
+// #include <iostream>
+// using namespace std;
+
+// class point {
+// public:
+//     int x, y;
+// };
+
+// point createpoint() {
+//     point p;
+//     p.x = 10;
+//     p.y = 20;
+
+//     return p;
+// }
+
+// int main() {
+//     point p = createpoint();
+
+//     cout << p.x << " " << p.y;
+
+//     return 0;
+// }
